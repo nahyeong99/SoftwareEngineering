8f0a1209b355210442361a13ec0fe251480bad7a
[suhyunie] <[sbsh0000@gmail.com]>
2021-05-19 01:23:46 +0900

    정보수정

---
+++ seTerm/app/src/main/java/gachon/mpclass/seterm/userEditInfo.java
@@ -0,0 +1,14 @@
+package gachon.mpclass.seterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userEditInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_edit_info);
+    }
+}
