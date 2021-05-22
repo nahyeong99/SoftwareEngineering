e8edb2a50878efd7866416647dd6e5d5fd1f1bf7
yekini9908 <82139188+yekini9908@users.noreply.github.com>
2021-05-23 02:11:27 +0900

    버튼 숨기기 2

--- seTerm/app/src/main/java/gachon/mpclass/seterm/FlowerListView_Adapter.java
+++ seTerm/app/src/main/java/gachon/mpclass/seterm/FlowerListView_Adapter.java
@@ -129,6 +129,52 @@ public class FlowerListView_Adapter extends BaseAdapter {
                         }
                     });
                     showButton(change);
+                    change.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                            View view = LayoutInflater.from(context).inflate(R.layout.activity_edit, null, false);
+                            builder.setView(view);
+                            final Button edit = (Button) view.findViewById(R.id.upload);
+                            final EditText name = (EditText) view.findViewById(R.id.flowername);
+                            final EditText color = (EditText) view.findViewById(R.id.flowercolor);
+                            final EditText num = (EditText) view.findViewById(R.id.flowernumber);
+
+                            name.setText(list.getFlowername());
+                            color.setText(list.getFlowercolor());
+                            num.setText(list.getFlowernumber());
+
+                            final AlertDialog dialog = builder.create();
+                            edit.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+
+                                    dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                            String editname = name.getText().toString();
+                                            String editcolor = color.getText().toString();
+                                            String editnum = num.getText().toString();
+                                            list.setFlowername(editname);
+                                            list.setFlowercolor(editcolor);
+                                            list.setFlowernumber(editnum);
+                                            dbRef.child(G.keyList.get(position)).child("flowername").setValue(editname);
+                                            dbRef.child(G.keyList.get(position)).child("flowercolor").setValue(editcolor);
+                                            dbRef.child(G.keyList.get(position)).child("flowernumber").setValue(editnum);
+                                            notifyDataSetChanged();
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError error) {
+
+                                        }
+                                    });
+                                    dialog.dismiss();
+                                }
+                            });
+                            dialog.show();
+                        }
+                    });
                 } else {
                     hideButton(delete);
                     hideButton(change);
@@ -139,52 +185,7 @@ public class FlowerListView_Adapter extends BaseAdapter {
         }
 
 
-        change.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                View view = LayoutInflater.from(context).inflate(R.layout.activity_edit, null, false);
-                builder.setView(view);
-                final Button edit = (Button) view.findViewById(R.id.upload);
-                final EditText name = (EditText) view.findViewById(R.id.flowername);
-                final EditText color = (EditText) view.findViewById(R.id.flowercolor);
-                final EditText num = (EditText) view.findViewById(R.id.flowernumber);
 
-                name.setText(list.getFlowername());
-                color.setText(list.getFlowercolor());
-                num.setText(list.getFlowernumber());
-
-                final AlertDialog dialog = builder.create();
-                edit.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                String editname = name.getText().toString();
-                                String editcolor = color.getText().toString();
-                                String editnum = num.getText().toString();
-                                list.setFlowername(editname);
-                                list.setFlowercolor(editcolor);
-                                list.setFlowernumber(editnum);
-                                dbRef.child(G.keyList.get(position)).child("flowername").setValue(editname);
-                                dbRef.child(G.keyList.get(position)).child("flowercolor").setValue(editcolor);
-                                dbRef.child(G.keyList.get(position)).child("flowernumber").setValue(editnum);
-                                notifyDataSetChanged();
-                            }
-
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError error) {
-
-                            }
-                        });
-                        dialog.dismiss();
-                    }
-                });
-                dialog.show();
-            }
-        });
 
         if (list.getImgUrl() == null)//사진이 없을때
         {
