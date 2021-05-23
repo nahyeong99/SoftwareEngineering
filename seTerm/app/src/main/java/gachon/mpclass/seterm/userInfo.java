1ffcf580f8ef031c873ba28e977bbd8607fd761f
nahyeong99 <nahyeong99@naver.com>
2021-05-24 00:26:46 +0900

    이름표시 수정

--- seTerm/app/src/main/java/gachon/mpclass/seterm/userInfo.java
+++ seTerm/app/src/main/java/gachon/mpclass/seterm/userInfo.java
@@ -1,51 +1,86 @@
 package gachon.mpclass.seterm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class userInfo extends AppCompatActivity {
-TextView uid;
+
+    String userName;
+    FirebaseAuth firebaseAuth;
+    private DatabaseReference mDatabase;
+
+    FirebaseUser user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
-        Button checkResrve = (Button)findViewById(R.id.checkReserve);
+
+        TextView name=(TextView)findViewById(R.id.userName);
+        firebaseAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        user = firebaseAuth.getCurrentUser();
+
+        mDatabase.child("Customers").child(user.getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                } else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                    userName = task.getResult().child("name").getValue().toString();
+                    name.setText(userName+"님");
+                }
+            }
+        });
+
+        Button checkResrve = (Button) findViewById(R.id.checkReserve);
         Button editInfoBtn = findViewById(R.id.editUserInfoBtn);
         Button reservationBtn = findViewById(R.id.reservation);
         checkResrve.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),userCheckReservation.class);
+                Intent intent = new Intent(getApplicationContext(), userCheckReservation.class);
                 startActivity(intent);
             }
         });
         editInfoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),userEditInfo.class);
+                Intent intent = new Intent(getApplicationContext(), userEditInfo.class);
                 startActivity(intent);
             }
         });
 
-        Button userLocation =(Button)findViewById(R.id.setLocation);
+        Button userLocation = (Button) findViewById(R.id.setLocation);
 
-        userLocation.setOnClickListener(new View.OnClickListener(){
+        userLocation.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
 
-                Intent intent = new Intent(getApplicationContext(),userLocation.class);
+                Intent intent = new Intent(getApplicationContext(), userLocation.class);
                 startActivity(intent);
             }
         });
         reservationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),userReservation.class);
+                Intent intent = new Intent(getApplicationContext(), userReservation.class);
                 startActivity(intent);
             }
         });
